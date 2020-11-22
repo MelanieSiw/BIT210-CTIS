69bc14baf71d0849e16f73f7dd02f9443dda29fe
Petmuel <49142438+Petmuel@users.noreply.github.com>
2020-11-22 21:14:25 +0800

    Update ctisdb.sql

--- php/ctisdb.sql
+++ php/ctisdb.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Nov 18, 2020 at 06:33 AM
--- Server version: 10.4.6-MariaDB
--- PHP Version: 7.1.31
+-- Host: 127.0.0.1
+-- Generation Time: Nov 22, 2020 at 02:11 PM
+-- Server version: 10.4.16-MariaDB
+-- PHP Version: 7.4.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -25,10 +24,10 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `covidTest`
+-- Table structure for table `covidtest`
 --
 
-CREATE TABLE `covidTest` (
+CREATE TABLE `covidtest` (
   `testID` int(5) NOT NULL,
   `testDate` date DEFAULT NULL,
   `result` varchar(30) DEFAULT NULL,
@@ -39,33 +38,38 @@ CREATE TABLE `covidTest` (
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `covidTest`
+-- Dumping data for table `covidtest`
 --
 
-INSERT INTO `covidTest` (`testID`, `testDate`, `result`, `resultDate`, `status`, `tester_username`, `patient_username`) VALUES
-(1, NULL, 'negative', '2020-11-17', 'completed', 'Mel', 'Dom'),
+INSERT INTO `covidtest` (`testID`, `testDate`, `result`, `resultDate`, `status`, `tester_username`, `patient_username`) VALUES
+(1, NULL, 'negative', '2020-11-17', 'completed', 'Clorfix', 'Dom'),
 (2, NULL, 'Positive', '2020-11-17', 'completed', 'Mel', 'Dylan'),
-(3, '2020-11-17', NULL, NULL, 'pending', 'Mel', 'Melanie'),
-(4, '2020-11-17', 'Positive', '2020-11-17', 'completed', 'Mel', 'Ben');
+(3, '2020-11-17', NULL, NULL, 'pending', 'Clorfix', 'Melanie'),
+(4, '2020-11-17', 'Positive', '2020-11-17', 'completed', 'Mel', 'Ben'),
+(5, '2020-11-18', 'in progress', NULL, 'pending', 'Mel', 'John'),
+(6, '2020-11-20', 'Positive', '2020-11-20', 'completed', 'Mel', 'Karl'),
+(7, '2020-11-20', 'Positive', '2020-11-20', 'completed', 'Mel', 'Michelle');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `TestCentre`
+-- Table structure for table `testcentre`
 --
 
-CREATE TABLE `TestCentre` (
+CREATE TABLE `testcentre` (
   `centreID` int(5) NOT NULL,
   `centreName` varchar(50) NOT NULL,
   `manager_username` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `TestCentre`
+-- Dumping data for table `testcentre`
 --
 
-INSERT INTO `TestCentre` (`centreID`, `centreName`, `manager_username`) VALUES
-(1, 'Sunway', 'Sam');
+INSERT INTO `testcentre` (`centreID`, `centreName`, `manager_username`) VALUES
+(1, 'Sunway', 'Sam'),
+(2, 'Puchong', 'Daniel'),
+(3, 'Subang', 'Sam');
 
 -- --------------------------------------------------------
 
@@ -91,10 +95,10 @@ INSERT INTO `testkit` (`kitID`, `kitName`, `stock`) VALUES
 -- --------------------------------------------------------
 
 --
--- Table structure for table `User`
+-- Table structure for table `user`
 --
 
-CREATE TABLE `User` (
+CREATE TABLE `user` (
   `username` varchar(100) NOT NULL,
   `password` varchar(20) NOT NULL,
   `fullname` varchar(100) DEFAULT NULL,
@@ -102,38 +106,45 @@ CREATE TABLE `User` (
   `symptoms` varchar(50) DEFAULT NULL,
   `position` varchar(30) DEFAULT NULL,
   `userType` varchar(30) NOT NULL,
-  `registeredBy` varchar(100) DEFAULT NULL
+  `registeredBy` varchar(100) DEFAULT NULL,
+  `workplace` int(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `User`
+-- Dumping data for table `user`
 --
 
-INSERT INTO `User` (`username`, `password`, `fullname`, `patientType`, `symptoms`, `position`, `userType`, `registeredBy`) VALUES
-('Ben', '123', 'Siw', 'Returnee', 'Flu, ', NULL, 'p', 'Mel'),
-('Dom', '1', 'Lee', 'Infected', 'Sore Throat,Dizzy', NULL, 'p', 'Mel'),
-('Dylan', '123', 'Chng', 'Suspected', 'Flu, Cough, Fever, Diarrhe ', NULL, 'p', 'Mel'),
-('Han123', '123', '1234', NULL, NULL, NULL, 't', 'Sam'),
-('Mel', '1', NULL, NULL, NULL, NULL, 't', NULL),
-('Melanie', '12345', 'Chng', '0', 'Cough, ', NULL, 'p', 'Mel'),
-('Sam', '1', NULL, NULL, NULL, NULL, 'm', NULL);
+INSERT INTO `user` (`username`, `password`, `fullname`, `patientType`, `symptoms`, `position`, `userType`, `registeredBy`, `workplace`) VALUES
+('Ben', '123', 'Siw', 'Returnee', 'Flu, ', NULL, 'p', 'Mel', NULL),
+('Clorfix', 'jacky', 'Jacky Voo', NULL, NULL, NULL, 't', 'Sam', 2),
+('Daniel', '123', NULL, NULL, NULL, NULL, 'm', NULL, 2),
+('Dom', '1', 'Lee', 'Infected', 'Sore Throat,Dizzy', NULL, 'p', 'Mel', NULL),
+('Dylan', '123', 'Chng', 'Suspected', 'Flu, Cough, Fever, Diarrhe ', NULL, 'p', 'Mel', NULL),
+('Han123', '123', '1234', NULL, NULL, NULL, 't', 'Sam', 1),
+('John', '123', 'John Cena', 'Quarantined', 'Cough, ', NULL, 'p', 'Mel', NULL),
+('Karl', '123', 'KarlChoo', 'Infected', 'Flu, Cough, Fever, Sore Throat, ', NULL, 'p', 'Mel', NULL),
+('kim', '123', 'kim123', NULL, NULL, NULL, 't', 'Sam', NULL),
+('Mel', '1', NULL, NULL, NULL, NULL, 't', 'Sam', 1),
+('Melanie', '12345', 'Chng', '0', 'Cough, ', NULL, 'p', 'Mel', NULL),
+('Michelle', '123', 'Michelle123', 'Quarantined', 'Flu, Cough, ', NULL, 'p', 'Mel', NULL),
+('Sam', '1', NULL, NULL, NULL, NULL, 'm', NULL, 1);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `covidTest`
+-- Indexes for table `covidtest`
 --
-ALTER TABLE `covidTest`
+ALTER TABLE `covidtest`
   ADD PRIMARY KEY (`testID`),
   ADD KEY `covidTest_user_fk2` (`patient_username`),
   ADD KEY `covidTest_user_fk1` (`tester_username`) USING BTREE;
 
 --
--- Indexes for table `TestCentre`
+-- Indexes for table `testcentre`
 --
-ALTER TABLE `TestCentre`
+ALTER TABLE `testcentre`
   ADD PRIMARY KEY (`centreID`),
   ADD KEY `TestCentre_user_fk1` (`manager_username`);
 
@@ -144,50 +155,57 @@ ALTER TABLE `testkit`
   ADD PRIMARY KEY (`kitID`);
 
 --
--- Indexes for table `User`
+-- Indexes for table `user`
 --
-ALTER TABLE `User`
+ALTER TABLE `user`
   ADD PRIMARY KEY (`username`),
-  ADD KEY `username` (`username`);
+  ADD KEY `username` (`username`),
+  ADD KEY `centre_manager` (`workplace`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `covidTest`
+-- AUTO_INCREMENT for table `covidtest`
 --
-ALTER TABLE `covidTest`
-  MODIFY `testID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+ALTER TABLE `covidtest`
+  MODIFY `testID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
--- AUTO_INCREMENT for table `TestCentre`
+-- AUTO_INCREMENT for table `testcentre`
 --
-ALTER TABLE `TestCentre`
-  MODIFY `centreID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+ALTER TABLE `testcentre`
+  MODIFY `centreID` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `testkit`
 --
 ALTER TABLE `testkit`
-  MODIFY `kitID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `kitID` int(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `covidTest`
+-- Constraints for table `covidtest`
 --
-ALTER TABLE `covidTest`
-  ADD CONSTRAINT `covidTest_user_fk1` FOREIGN KEY (`tester_username`) REFERENCES `User` (`username`),
-  ADD CONSTRAINT `covidTest_user_fk2` FOREIGN KEY (`patient_username`) REFERENCES `User` (`username`);
+ALTER TABLE `covidtest`
+  ADD CONSTRAINT `covidTest_user_fk1` FOREIGN KEY (`tester_username`) REFERENCES `user` (`username`),
+  ADD CONSTRAINT `covidTest_user_fk2` FOREIGN KEY (`patient_username`) REFERENCES `user` (`username`);
 
 --
--- Constraints for table `TestCentre`
+-- Constraints for table `testcentre`
 --
-ALTER TABLE `TestCentre`
-  ADD CONSTRAINT `TestCentre_user_fk1` FOREIGN KEY (`manager_username`) REFERENCES `User` (`username`);
+ALTER TABLE `testcentre`
+  ADD CONSTRAINT `TestCentre_user_fk1` FOREIGN KEY (`manager_username`) REFERENCES `user` (`username`);
+
+--
+-- Constraints for table `user`
+--
+ALTER TABLE `user`
+  ADD CONSTRAINT `centre_manager` FOREIGN KEY (`workplace`) REFERENCES `testcentre` (`centreID`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
